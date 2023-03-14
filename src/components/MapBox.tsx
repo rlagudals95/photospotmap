51ea750ca5752b8d3cc4cd28b4d8f0a5b0931a6f
gudals95 <102935090+gudals95@users.noreply.github.com>
2023-03-15 00:09:49 +0900

    feat: 위도 경도 계산 마커 플러스 중..

--- src/components/MapBox.tsx
+++ src/components/MapBox.tsx
@@ -1,34 +1,68 @@
-import { useEffect, useRef, useState, memo } from 'react';
-import mapboxgl from 'mapbox-gl';
-import styled from '@emotion/styled';
+import { useEffect, useRef, useState, useCallback, memo } from "react";
+import mapboxgl from "mapbox-gl";
+import styled from "@emotion/styled";
 
 function MapBox() {
   const [lng, setLng] = useState(126.977);
   const [lat, setLat] = useState(37.566);
   const [zoom, setZoom] = useState(10);
+  const [map, setMap] = useState<mapboxgl.Map>();
+
   const mapContainer = useRef(null);
 
-  useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoicmxhZ3VkYWxzOTUiLCJhIjoiY2xmOGNoYTZnMTBiMDNxcG8xNXY5eTcxMiJ9.oCf7k9VWF4pwEL0fkPJt1w';
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-
-
-    return () => map.remove();
+  const loadMaxBox = useCallback(async () => {
+    try {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoicmxhZ3VkYWxzOTUiLCJhIjoiY2xmOGNoYTZnMTBiMDNxcG8xNXY5eTcxMiJ9.oCf7k9VWF4pwEL0fkPJt1w";
+      const loadedMap = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [lng, lat],
+        zoom: zoom,
+      });
+      setMap(loadedMap);
+      return loadedMap;
+    } catch (error) {
+      console.error(`map box load error: ${error}`);
+    }
   }, []);
 
-  return (
-    <Container ref={mapContainer} className="mapContainer"></Container>
-  );
+  const loadMarkers = useCallback(async (loadedMap: mapboxgl.Map) => {
+    try {
+      new mapboxgl.Marker().setLngLat([126.9518, 37.4782]).addTo(loadedMap);
+    } catch (error) {
+      console.error(`load marker error: ${error}`);
+    }
+  }, []);
+
+  const addMouseMoveEvent = useCallback(async (loadedMap: mapboxgl.Map) => {
+    loadedMap.on("mousemove", (e) => {
+      console.log(e.lngLat.wrap());
+    });
+  }, []);
+
+  useEffect(() => {
+    const fetchMapData = async () => {
+      const loadedMap = (await loadMaxBox()) as mapboxgl.Map;
+      await loadMarkers(loadedMap);
+      await addMouseMoveEvent(loadedMap);
+    };
+    fetchMapData();
+
+    return () => {
+      if (map) {
+        //@ts-ignore
+        map.remove();
+      }
+    };
+  }, []);
+
+  return <Container ref={mapContainer} className="mapContainer"></Container>;
 }
 
-export default memo(MapBox)
+export default memo(MapBox);
 
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
-`
+`;
